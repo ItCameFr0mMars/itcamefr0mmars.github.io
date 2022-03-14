5f9fb987bd607a2d5959dce3ff03039de4d5dc73
ItCameFr0mMars <m4rsgamingyt@gmail.com>
2022-03-14 14:20:06 +0000

    slither

---
+++ games/slitherio/js.js
@@ -0,0 +1,4 @@
+var front = 'https://nkalloy';
+var end = '.up.railway.app/web/_aHR0cDovL3NsaXRoZXIuaW8=_/'
+var el = document.getElementById('ifrm');
+el.src = front+num+end; // assign url to src property
