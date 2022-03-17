9b68b31c0596465187ff7f350df2349c85b44c61
ItCameFr0mMars <m4rsgamingyt@gmail.com>
2022-03-17 13:43:49 +0000

    tons of new games

--- games/slitherio/js.js
+++
@@ -1,4 +0,0 @@
-var front = 'https://nkalloy';
-var end = '.up.railway.app/web/_aHR0cDovL3NsaXRoZXIuaW8=_/'
-var el = document.getElementById('ifrm');
-el.src = front+num+end; // assign url to src property
